d98facf0b87778c7928b9a09c296525969287f03
Guillermo Riaño <guilledark17@gmail.com>
2023-06-19 11:38:28 -0600

    ✅ Create Document to PDF

--- ConsoleApp_PDF/Program.cs
+++ ConsoleApp_PDF/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Previewer;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Creae new Document
+Document.Create(document =>
+{
+    document.Page(page =>
+    {
+        // page content
+    });
+}).ShowInPreviewer();
