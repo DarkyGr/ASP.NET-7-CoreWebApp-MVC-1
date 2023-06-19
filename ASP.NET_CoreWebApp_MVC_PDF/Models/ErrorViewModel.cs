e370884cf8ce39e7407a51c6ba82a0f73f9f4fa5
Guillermo Riaño <guilledark17@gmail.com>
2023-06-19 11:26:57 -0600

    🎉 Initial: Adding Project Folder

---
+++ ASP.NET_CoreWebApp_MVC_PDF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_CoreWebApp_MVC_PDF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
