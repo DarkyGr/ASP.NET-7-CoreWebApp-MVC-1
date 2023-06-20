c818ef9dbccaa78b00f165afd0c84e46c623483d
Guillermo Riaño <guilledark17@gmail.com>
2023-06-20 13:19:44 -0600

    ✔️ Create Button of Download

--- ASP.NET_CoreWebApp_MVC_PDF/Program.cs
+++ ASP.NET_CoreWebApp_MVC_PDF/Program.cs
@@ -1,7 +1,11 @@
+using QuestPDF.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+QuestPDF.Settings.License = LicenseType.Community;
 
 var app = builder.Build();
 
